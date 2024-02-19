521ef37739a89453c30e85aaea2bdbbf5d75f41b
Vladimir DOMBROVSKI <vladimir.dombrovski@bso.co>
2024-02-19 14:34:01 +0100

    Cleaner: fix concurrent map writes

--- pkg/bastion/cleaner.go
+++ pkg/bastion/cleaner.go
@@ -7,10 +7,64 @@ import (
 	"log"
 	"moul.io/sshportal/pkg/dbmodels"
 	"time"
+	"sync"
 )
 
-var ActiveConnections = map[uint]map[uint]*gossh.ServerConn{}
-var ActiveSessions = map[uint]map[uint]ssh.Session{}
+type ConnectionManager struct {
+	sync.RWMutex
+	ActiveConnections map[uint]map[uint]*gossh.ServerConn
+	ActiveSessions map[uint]map[uint]ssh.Session
+}
+
+func MakeConnectionManager() *ConnectionManager {
+	return &ConnectionManager{
+		ActiveConnections: map[uint]map[uint]*gossh.ServerConn{},
+		ActiveSessions: map[uint]map[uint]ssh.Session{},
+	}
+}
+
+func (cm *ConnectionManager) GetActiveConnections(userID uint) (map[uint]*gossh.ServerConn, bool) {
+	cm.RLock()
+	cnx, ok := cm.ActiveConnections[userID]
+	cm.RUnlock()
+	return cnx, ok
+}
+
+func (cm *ConnectionManager) AddConnection(userID, sid uint, conn *gossh.ServerConn) {
+	cm.Lock()
+	if _, ok := cm.ActiveConnections[userID]; !ok {
+		cm.ActiveConnections[userID] = map[uint]*gossh.ServerConn{}
+	}
+	cm.ActiveConnections[userID][sid] = conn
+	cm.Unlock()
+}
+
+func (cm *ConnectionManager) DelConnection(userID, sid uint) {
+	cm.Lock()
+	delete(cm.ActiveConnections[userID], sid)
+	cm.Unlock()
+}
+
+func (cm *ConnectionManager) GetActiveSessions(userID uint) (map[uint]ssh.Session, bool) {
+	cm.RLock()
+	sess, ok := cm.ActiveSessions[userID]
+	cm.RUnlock()
+	return sess, ok
+}
+
+func (cm *ConnectionManager) AddSession(userID, sid uint, sess ssh.Session) {
+	cm.Lock()
+
+	cm.Unlock()
+}
+
+func (cm *ConnectionManager) DelSession(userID, sid uint) {
+	cm.Lock()
+	delete(cm.ActiveSessions[userID], sid)
+	cm.Unlock()
+}
+
+var CnxManager = MakeConnectionManager()
 
 const USER_DISABLED = "DISABLED"
 
@@ -22,16 +76,16 @@ func CleanConnections(db *gorm.DB) {
 		}
 
 		for _, user := range bannedUsers {
-			if connections, ok := ActiveConnections[user.ID]; ok {
+			if connections, ok := CnxManager.GetActiveConnections(user.ID); ok {
 				for sid, cnx := range connections {
 					cnx.Close()
-					delete(ActiveConnections[user.ID], sid)
+					CnxManager.DelConnection(user.ID, sid)
 				}
 			}
-			if sessions, ok := ActiveSessions[user.ID]; ok {
+			if sessions, ok := CnxManager.GetActiveSessions(user.ID); ok {
 				for sid, sess := range sessions {
 					sess.Close()
-					delete(ActiveSessions[user.ID], sid)
+					CnxManager.DelSession(user.ID, sid)
 				}
 			}
 		}
